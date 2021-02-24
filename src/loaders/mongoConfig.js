20b7c0c885c6b53816a724ae5874e0abf989ea4e
Piyush jethania <76402703+pjethania99@users.noreply.github.com>
2021-02-24 11:19:25 +0530

    Delete mongoConfig.js

--- src/loaders/mongoConfig.js
+++
@@ -1,9 +0,0 @@
-var mongoose=require('mongoose');
-var config=require('../config');
-
-const mongoConfig=async ()=>{
-    const connection=await mongoose.connect(config.databaseURL,()=>console.log('connected to db'),{useNewUrlParser:true,createIndexes:true});
-    return connection.connection.db;
-};
-
-module.exports=mongoConfig;
